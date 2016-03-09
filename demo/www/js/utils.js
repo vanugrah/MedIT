baf22cc69ee7548eedba63bdb09175d54e3f9aff
Anthony Tsou <aytsou147@gmail.com>
2016-03-09 16:09:18 -0500

    Added comments to localstorage functions

--- demo/www/js/utils.js
+++ demo/www/js/utils.js
@@ -5,15 +5,19 @@ angular.module('ionic.utils', [])
 
   .factory('$localstorage', ['$window', function($window) {
     return {
+      // Saves a string value in localstorage with specified key
       set: function(key, value) {
         $window.localStorage[key] = value;
       },
+      // Returns a string value from localstorage associated with specified key
       get: function(key, defaultValue) {
         return $window.localStorage[key] || defaultValue;
       },
+      // Saves an obj in localstorage with specified key
       setObject: function(key, value) {
         $window.localStorage[key] = JSON.stringify(value);
       },
+      // Returns an obj from localstorage associated with specified key
       getObject: function(key) {
         return JSON.parse($window.localStorage[key] || '{}');
       }
