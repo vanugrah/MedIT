d289dd020be3f9128f518c3e5bfd5cde1165d53b
Anugrah Vijay <cooldude_anugrah@hotmail.com>
2016-03-30 17:27:06 -0400

    Added login page

--- demo/www/js/app.js
+++ demo/www/js/app.js
@@ -1,10 +1,4 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('medIT', ['ionic', 'ionic.utils', 'medIT.controllers', 'medIT.home'])
+angular.module('medIT', ['ionic', 'ionic.utils', 'medIT.controllers', 'medIT.home', 'medIT.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -25,7 +19,13 @@ angular.module('medIT', ['ionic', 'ionic.utils', 'medIT.controllers', 'medIT.hom
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-    .state('app', {
+  .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'LoginCtrl'
+  })
+
+  .state('app', {
     url: '/app',
     abstract: true,
     templateUrl: 'templates/menu.html',
@@ -69,6 +69,7 @@ angular.module('medIT', ['ionic', 'ionic.utils', 'medIT.controllers', 'medIT.hom
       }
     }
   });
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/home');
+  $urlRouterProvider.otherwise('/login');
 })
---
+++ demo/www/js/services.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Anugrah Vijay on 03/30/2016.
+ */
+angular.module('medIT.services', [])
+
+// Angular service to authenticate login
+.service('LoginService', function($q) {
+    return {
+        loginUser: function(name, pw) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+ 
+            if (name == 'user' && pw == 'secret') {
+                deferred.resolve('Welcome ' + name + '!');
+            } else {
+                deferred.reject('Wrong credentials.');
+            }
+            promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+        }
+    }
+});
