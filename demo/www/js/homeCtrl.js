b7835000535823a075ceedeb5b08014ab2cc457b
Anugrah Vijay <cooldude_anugrah@hotmail.com>
2016-04-11 16:13:51 -0400

    Updated home screen

--- demo/www/js/homeCtrl.js
+++ demo/www/js/homeCtrl.js
@@ -1,18 +1,16 @@
-/**
- * Created by Anthony Tsou on 3/7/2016.
- */
 // Home Controller
 angular.module('medIT.home', [])
 
   .controller('HomeCtrl', function($scope, $localstorage) {
 
     $scope.appts = [
-      { title: 'Appointment 1', id: 1 },
-      { title: 'Appointment 2', id: 2 },
-      { title: 'Appointment 3', id: 3 },
-      { title: 'Appointment 4', id: 4 },
-      { title: 'Appointment 5', id: 5 },
-      { title: 'Appointment 6', id: 6 }
+      { title: 'Appointment 1', 
+        id: 1,
+        location: "Children's Healthcare of Atlanta",
+        date: "15th April 2016",
+        physician: "Dr. Batisky"
+      },
+      
     ];
 
     $scope.viewAppt = function(appt) {
