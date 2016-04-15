323823875031a2d66d02bee43b0cf980ba526e68
Anthony Tsou <aytsou147@gmail.com>
2016-04-15 16:28:21 -0400

    Wrote sample HTTP GET request for getting user prefernces on settings page load. Commented for now because we need real URL path.

--- demo/www/js/settingsCtrl.js
+++ demo/www/js/settingsCtrl.js
@@ -4,16 +4,29 @@
 // Settings Controller
 angular.module('medIT.settings', [])
 
-  .controller('SettingsCtrl', function($scope, $localstorage) {
+  .controller('SettingsCtrl', function($scope, $http) {
 
-    // HTTP GET request that will retrieve values
     $scope.push = true;
     $scope.sms = true;
     $scope.email = true;
     $scope.weather = true;
     $scope.traffic = true;
 
+    // HTTP GET request that will retrieve values
     $scope.$on('$ionicView.beforeEnter', function(){
+
+        //$http.get("http://127.0.0.1/{REST OF THE URL PLS}")
+        //  .success(function(data) {
+        //    $scope.push = data.push;
+        //    $scope.sms = data.sms;
+        //    $scope.email = data.email;
+        //    $scope.weather = data.weather;
+        //    $scope.traffic = data.traffic;
+        //  })
+        //  .error(function(data) {
+        //    alert("You messed up");
+        //  });
+
       // Retrieve all preferences from DB using GET request
       //alert("Settings: "
       //  + "\nPush: " + $scope.push
@@ -41,12 +54,12 @@ angular.module('medIT.settings', [])
     };
 
     $scope.$on('$ionicView.beforeLeave', function(){
-      //alert("Settings: "
-      //  + "\nPush: " + $scope.push
-      //  + "\nSMS: " + $scope.sms
-      //  + "\nEmail: " + $scope.email
-      //  + "\nWeather: " + $scope.weather
-      //  + "\nTraffic: " + $scope.traffic);
+      alert("Settings: "
+        + "\nPush: " + $scope.push
+        + "\nSMS: " + $scope.sms
+        + "\nEmail: " + $scope.email
+        + "\nWeather: " + $scope.weather
+        + "\nTraffic: " + $scope.traffic);
 
       // Save all settings in DB using POST req
     });
