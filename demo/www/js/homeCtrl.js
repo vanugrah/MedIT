7b056f58e0d883a6d7d98432ad9e0f3622b7fd2b
Anthony Tsou <aytsou147@gmail.com>
2016-04-19 01:39:33 -0400

    Reformatted appt list and added colored bar on left side to denote which child. Reformatted appt information page. Added new CSS classes.

--- demo/www/js/homeCtrl.js
+++ demo/www/js/homeCtrl.js
@@ -8,29 +8,44 @@ angular.module('medIT.home', [])
         {
           id: 1,
           location: "Children's Healthcare of Atlanta - Egleston",
-          address: "1405 Clifton Road NE, Atlanta, GA 30322",
+          address: "1405 Clifton Road NE",
+          city: "Atlanta",
+          state: "GA",
+          zip: "30322",
           date: "21 April 2016",
           time: "11:00am",
           physician: "Dr. Batisky",
-          patient: "Johnny Doe Jr."
+          patient: "Johnny Doe Jr.",
+          patientID: "0",
+          notes: "Follow-up appointment"
         },
         {
           id: 2,
           location: "Children's Healthcare of Atlanta - Egleston",
-          address: "1405 Clifton Road NE, Atlanta, GA 30322",
+          address: "1405 Clifton Road NE",
+          city: "Atlanta",
+          state: "GA",
+          zip: "30322",
           date: "28 April 2016",
           time: "1:00pm",
           physician: "Dr. Omojokun",
-          patient: "Johanna Doe"
+          patient: "Johanna Doe",
+          patientID: "1",
+          notes: "6-month check-up"
         },
         {
           id: 3,
           location: "Children's Healthcare of Atlanta - Egleston",
-          address: "1405 Clifton Road NE, Atlanta, GA 30322",
+          address: "1405 Clifton Road NE",
+          city: "Atlanta",
+          state: "GA",
+          zip: "30322",
           date: "05 May 2016",
           time: "8:00am",
           physician: "Dr. Menagarishvili",
-          patient: "Johnny Doe Jr."
+          patient: "Johnny Doe Jr.",
+          patientID: "0",
+          notes: "N/A"
         }
       ];
 
